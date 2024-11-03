1fbfd323f904f246bc841e69e3471c1d0631b4fb
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-11-02 23:41:59 -0400

    image preview

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const SearchResult = ({ result, error, onBack, onRegister }) => {
+const SearchResult = ({ image, result, error, onBack, onRegister }) => {
   return (
     <motion.div 
       initial={{ opacity: 0, x: 300 }}
@@ -16,6 +16,18 @@ const SearchResult = ({ result, error, onBack, onRegister }) => {
             {/* Display match results */}
             <div className="space-y-4">
               <div className="grid grid-cols-2 gap-4">
+              <div className="bg-white/10 backdrop-blur-lg rounded-lg p-6">
+            {image && (
+                <div className="mb-6">
+                    <img 
+                    src={URL.createObjectURL(image)} 
+                    alt="Upload Preview" 
+                    className="w-32 h-32 object-cover rounded-lg mx-auto"
+                    />
+                </div>
+                )}
+                </div>
+                <br />
                 <div>
                   <label className="block text-gray-400 text-sm">Name</label>
                   <p>{result.firstName} {result.lastName}</p>
